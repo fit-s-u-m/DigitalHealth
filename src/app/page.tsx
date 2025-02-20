dd86a8a9948db924caaf99b87838ba73e5364e6a
fit-s-u-m <nehemiahmaths@gmail.com>
2025-02-21 01:33:07 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import { FetchName } from "@/components/FetchName";
-
-export default async function Home() {
-  return (
-    <>
-      <h2 className="underline">
-        <FetchName />
-      </h2>
-    </>
-  );
+export default function Home() {
+  return <div className="underline text-3xl font-bold text-blue-800">Button</div>;
 }
