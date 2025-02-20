38882f5722c3d9bb419ccb4d5118a683838dbd80
Fikresilase <shitubilbila@gmail.com>
2025-02-21 00:50:21 +0300

    tailwind finally

---
+++ src/app/patients/page.tsx
@@ -0,0 +1,8 @@
+import PatientCard from "@/components/patientCard"
+
+function Patients (){
+    return<>
+    <PatientCard/>
+</>
+}
+export default Patients
---
+++ src/components/patientCard.tsx
@@ -0,0 +1,9 @@
+function PatientCard (){
+    return<>
+    <div className="bg-white shadow-md m-4 rounded-lg p-6 max-w-xs">
+    <h2 className="text-gray-600 text-sm">Total Sales</h2>
+    <p className="text-3xl font-bold text-gray-800">1,234</p>
+</div>
+</>
+}
+export default PatientCard
