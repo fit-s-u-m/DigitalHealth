5ad0460d16a4cd86b1d6f81f0571f9609cd85345
fit-s-u-m <nehemiahmaths@gmail.com>
2025-02-21 00:53:10 +0300

    added tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { FetchName } from "@/components/FetchName";
 export default async function Home() {
   return (
     <>
-      <h2>
+      <h2 className="underline">
         <FetchName />
       </h2>
     </>
