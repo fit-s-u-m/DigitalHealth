55fa7eab607b86ae80c06541fba01818ee817b12
haile <haileleulfiseha@gmail.com>
2025-02-20 22:48:20 +0300

    trial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { LatestMissionName } from "@/components/LatestMissionName";
+// import { LatestMissionName } from "@/components/LatestMissionName";
 
 export default async function Home() {
   return (
     <>
       <article>
         <h2>
-          Latest mission: <LatestMissionName />
+          {/* Latest mission: <LatestMissionName /> */}
         </h2>
       </article>
     </>
